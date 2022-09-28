a1df97227e618f31642ae49555f49f8b2484a8e0
anna-ermakova <korovanna8@mail.ru>
2022-09-28 14:42:49 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+
+    //task 1
+    System.out.println("Задание 1");
+    int age=50;
+    if (age>=18) {System.out.println("Поздравляю, ты совершеннолетний!!!");}
+    if (age<18) {System.out.println("До совершеннолетия нужно еще подождать...");}
+           }
 }
