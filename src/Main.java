a6a703d852524fb143ff80043b7d802e5c0f7bee
anna-ermakova <korovanna8@mail.ru>
2022-09-28 16:57:23 +0300

    Задание 1-2

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,22 @@ public class Main {
     public static void main(String[] args) {
 
     //task 1
-    System.out.println("Задание 1");
+    System.out.println("ДЗ1. Задание 1");
     int age=50;
     if (age>=18) {System.out.println("Поздравляю, ты совершеннолетний!!!");}
     if (age<18) {System.out.println("До совершеннолетия нужно еще подождать...");}
+
+    //task 2
+    System.out.println("ДЗ1. Задание 2");
+    int ageOfThePerson=20;
+    if (ageOfThePerson>=7) {
+    if (ageOfThePerson<18)
+        System.out.println("Ребенок ходит в школу");}
+    if (ageOfThePerson>=18) {
+    if (ageOfThePerson<24)
+            System.out.println("Человек уже закончил школу и может отправляться в университет.");}
+    if (ageOfThePerson>24) {
+    System.out.println("Человек окончил университет и ему пора искать работу.");}
+
            }
-}
+    }
