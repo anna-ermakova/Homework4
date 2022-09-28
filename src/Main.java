3cd19f2071104c054939d1e1b2a7d4a6eb16d2ed
anna-ermakova <korovanna8@mail.ru>
2022-09-28 17:35:18 +0300

    Задание 1-3

--- src/Main.java
+++ src/Main.java
@@ -19,5 +19,22 @@ public class Main {
     if (ageOfThePerson>24) {
     System.out.println("Человек окончил университет и ему пора искать работу.");}
 
+    //task 3
+    System.out.println("ДЗ1. Задание 3");
+    int numberOfPassengers=1;
+    int allWagonPlace=102;
+    int sitWagonPlace=60;
+    int standWagonPlace=allWagonPlace-sitWagonPlace;
+    if (numberOfPassengers>=102){
+        System.out.println("Свободных мест нет!");}
+    if (numberOfPassengers>=60){
+        if (numberOfPassengers<102)
+        System.out.println("В вагоне свободно "+(allWagonPlace-numberOfPassengers)+" стоячих мест");}
+    if (numberOfPassengers<60){
+        System.out.println("В вагоне свободно "+(sitWagonPlace-numberOfPassengers)+" сидячих мест и "+(allWagonPlace-sitWagonPlace)+ " стоячих мест.");
+    }
+
+
+
            }
     }
