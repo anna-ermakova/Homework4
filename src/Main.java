759661a10b9d396167035c31478f0ecb629df8d8
anna-ermakova <korovanna8@mail.ru>
2022-09-29 14:26:23 +0300

    ДЗ2. Задание 2

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,25 @@ public class Main {
 
         //task 1
         System.out.println("ДЗ2. Задание 1");
-        int age=12;
-        if (age>=18) {
+        int age = 12;
+        if (age >= 18) {
             System.out.println("Поздравляю, ты совершеннолетний!!!");
-        } else {System.out.println("До совершеннолетия нужно еще подождать...");
+        } else {
+            System.out.println("До совершеннолетия нужно еще подождать...");
         }
+        //task 2
+        System.out.println("ДЗ2. Задание 2");
+        int ageOfThePerson = 2;
+        if (ageOfThePerson >24){
+                System.out.println("Человек окончил университет и ему пора искать работу.");
+        if (ageOfThePerson >= 18)
+                System.out.println("Человек уже закончил школу и может отправляться в университет.");
+        if (ageOfThePerson>=7);
+            System.out.println("Человек ходит в школу.");
+        }else {
+            System.out.println("Человек-дошкольник.");
+        }
+
     }
-}
+
+    }
