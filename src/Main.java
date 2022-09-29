a08003ba49c10fb631f45d9bd07850422e6fcc83
anna-ermakova <korovanna8@mail.ru>
2022-09-29 14:46:59 +0300

    ДЗ2. Задание 2 исправила

--- src/Main.java
+++ src/Main.java
@@ -12,16 +12,20 @@ public class Main {
         }
         //task 2
         System.out.println("ДЗ2. Задание 2");
-        int ageOfThePerson = 2;
+        int ageOfThePerson =20;
         if (ageOfThePerson >24){
-                System.out.println("Человек окончил университет и ему пора искать работу.");
-        if (ageOfThePerson >= 18)
-                System.out.println("Человек уже закончил школу и может отправляться в университет.");
-        if (ageOfThePerson>=7);
-            System.out.println("Человек ходит в школу.");
-        }else {
-            System.out.println("Человек-дошкольник.");
-        }
+                System.out.println("Человек окончил университет и ему пора искать работу.");}
+        if (ageOfThePerson <24)
+        if (ageOfThePerson>=18){
+                System.out.println("Человек уже закончил школу и может отправляться в университет.");}
+        if (ageOfThePerson<18)
+        if (ageOfThePerson>=7){
+            System.out.println("Человек ходит в школу.");}
+        else {
+            System.out.println("Человек-дошкольник.");}
+
+        //task 3
+        System.out.println("ДЗ2. Задание 3");
 
     }
 
